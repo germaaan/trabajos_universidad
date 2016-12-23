09235fae0cc706c2531fc444802b9b5d2fccaa73
Cecilia <ceci.merelo@gmail.com>
2016-12-23 18:22:09 +0100

    Faltan acentos

--- 1GII/FP/tema_02/src/ejercicio_14.cpp
+++ 1GII/FP/tema_02/src/ejercicio_14.cpp
@@ -1,7 +1,7 @@
 // Se pide leer un caracter desde teclado, obligando al usuario a que sea una letra ma-
-// yuscula. Para ello habra que usar una estructura repetitiva "do while", de forma que
-// si el usuario introduce un caracter que no sea una letra mayuscula, se le volvera a
-// pedir otro caracter. Calculad la minuscula correspondiente e imprimidla en pantalla.
+// yúscula. Para ello habrá que usar una estructura repetitiva "do while", de forma que
+// si el usuario introduce un carácter que no sea una letra mayúscula, se le volvera a
+// pedir otro carácter. Calculad la minúscula correspondiente e imprimidla en pantalla.
 // No pueden usarse las funciones "tolower" ni "toupper" de la biblioteca "cctype". Si se
 // quiere, se puede usar como base el proyecto que resolvio el ejercicio 14 de la relacion
 // de problemas I.
